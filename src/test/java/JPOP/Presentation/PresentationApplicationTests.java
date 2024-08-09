b670e58fefb9965a3ec6900a3534f12eb985e068
rafiou <rafiou.djako@ipnetinstitute.com>
2024-08-09 13:26:01 +0000

    first commit

---
+++ src/test/java/JPOP/Presentation/PresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package JPOP.Presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
